50d0146054e90d32b82bb486f0878a11f971bc20
aprishka <dennis@opryshko.com>
2022-09-13 18:37:14 +0300

    documentation

--- src/driver.py
+++ src/driver.py
@@ -6,6 +6,15 @@ Page = wikipedia.WikipediaPage
 
 
 def search_pages(of: str, top: int) -> [Page]:
+    """
+    Search for desired page, respecting disambiguation
+
+    :param of: The Wikipedia page to search for
+    :param top: The amount of results to return in case of a disambiguation
+    :return: List of Wikipedia pages
+    :raises DisambiguationError: Upon trying to access a recursive disambiguation page
+    :raises PageError: Upon failing to access a page in a disambiguation
+    """
     try:
         out = [wikipedia.page(of)]
     except wikipedia.DisambiguationError as disambiguation:
@@ -15,10 +24,23 @@ def search_pages(of: str, top: int) -> [Page]:
 
 
 def run_search(of: str, top: int) -> Response:
+    """
+    Search for desired page, respecting disambiguation, and construct response
+
+    :param of: The Wikipedia page to search for
+    :param top: The amount of results to return in case of a disambiguation
+    :return: SearchResponse
+    """
     pages   = search_pages(of, top)
     entries = list(map(page_to_entry, pages))
     return Response(results=entries)
 
 
 def page_to_entry(page: Page) -> Entry:
+    """
+    Extract title and summary from Wikipedia page
+
+    :param page: The relevant page
+    :return: page title and summary
+    """
     return Entry(title=page.title, extract=page.summary)
